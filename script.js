b6553dcd562fe6455061623e483f492172cdc0a1
Chad Harper <chadharper811@gmail.com>
2024-07-14 18:53:49 -0500

    finished highPrecedence func

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const infixToFunction = {
 const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1),parseFloat(arg2)));
 
 const highPrecedence = (str) =>{
-    const regex = /([\d.]+)([*\/])([\d.]+)/
-    return regex.test(str);
-
+    const regex = /([\d.]+)([*\/])([\d.]+)/;
+    const str2 = infixEval(str, regex);
+    return str2 === str ? str : highPrecedence(str2);
 }
 
 const isEven = (num) => num%2 === 0;
