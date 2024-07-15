df0875a4a2c7cdcc8ce891230ee48f56849d7c4d
Chad Harper <chadharper811@gmail.com>
2024-07-14 19:52:35 -0500

    finished functions for speadsheeetfunction

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ const highPrecedence = (str) =>{
     return str2 === str ? str : highPrecedence(str2);
 }
 
-const isEven = (num) => num%2 === 0;
-const sum = (nums) => nums.reduce((acc,el) => acc + el, 0);
-const average = (nums) => sum(nums)/nums.length;
-const median = (nums) => {
-    const sorted = nums.slice().sort((a,b) => a-b);
+const isEven = num => num % 2 === 0;
+const sum = nums => nums.reduce((acc,el) => acc + el, 0);
+const average = nums => sum(nums) / nums.length;
+const median = nums => {
+    const sorted = nums.slice().sort((a,b) => a - b);
     const length = sorted.length;
     const middle = length / 2 - 1;
     return isEven(length) ? average([sorted[middle],sorted[middle +1]]) : sorted[Math.ceil(middle)];
@@ -28,10 +28,16 @@ const spreadsheetFunctions = {
     average,
     median,
     even: nums => nums.filter(isEven),
+    someeven: nums => nums.some(isEven),
+    everyeven: nums => nums.every(isEven),
     firsttwo: nums => nums.slice(0,2),
     lasttwo: nums => nums.slice(-2),
     has2: nums => nums.includes(2),
     increment: nums => nums.map(num => num + 1),
+    random: ([x, y]) => Math.floor(Math.random() * y + x),
+    range: nums => range(...nums),
+    nodupes: nums => [...new Set(nums).values()],
+    "": arg => arg
 }
 
 const applyFunction = (str) => {
