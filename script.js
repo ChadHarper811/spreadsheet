f0b30fef4e33ad8455ab3bdea12e8be8c16b132f
Chad Harper <chadharper811@gmail.com>
2024-07-14 19:12:37 -0500

    finished applyFunction

--- script.js
+++ script.js
@@ -35,7 +35,8 @@ const applyFunction = (str) => {
     const str2 = infixEval(noHigh,infix);
     const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
     const toNumberList = (args) => args.split(",").map(parseFloat);
-    const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()]
+    const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+    return str2.replace(functionCall, (_match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn,args) : match);
 };
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
